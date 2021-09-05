ff6d153e6a0be76e6650111133be881d61b782f6
kaisei <63438515+lemolatoon@users.noreply.github.com>
2021-09-05 15:06:33 +0900

    set values

--- titration.py
+++ titration.py
@@ -1,3 +1,4 @@
+from os import name
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,13 +20,13 @@ def h2so4(ph: np.ndarray, graph: bool = False):
     print(f"\npH:\n{ph}")
 
     da, dha, dh2a = calc_mol(h, k2)
-    print(f"\nモル分率\nSO4:\n{da}\nHSO4:\n{dha}\nH2SO4:\n{dh2a}")
+    print(f"\nモル分率\nSO4:\n{da * 100}\nHSO4:\n{dha*100}\nH2SO4:\n{dh2a*100}")
 
     n = n_bar(dha, dh2a)
 
     print(f"\n平均プロトン数:\n{n}")
 
-    vb = calc_vb(va, ca, dha, dh2a, h, oh, cb)
+    vb = calc_vb2(ca, va, n, cb, h, oh)
 
     print(f"\n滴下量:\n{vb}")
 
@@ -37,15 +38,26 @@ def h2so4(ph: np.ndarray, graph: bool = False):
 
     a = calc_a(n, h, oh, ca)
 
+
     if graph:
 
         plt.plot(ph, vb)
         plt.ylim(0, 20)
+        plt.title("vb")
         plt.show()
 
         plt.plot(ph, da)
         plt.plot(ph, dha)
         plt.plot(ph, dh2a)
+        plt.title("mol %")
+        plt.show()
+
+        plt.plot(ph, np.log(alpha))
+        plt.title("log alpha")
+        plt.show()
+
+        plt.plot(ph, n)
+        plt.title("n")
         plt.show()
 
 
@@ -116,6 +128,9 @@ def n_bar(dha, dh2a):
     return dha + 2 * dh2a
 
 def calc_vb(va, ca, dha, dh2a, h, oh, cb):
+    """
+    should not use
+    """
     vb = (va * (2 * ca - dha - 2 * dh2a - h + oh)) / (cb + dha + 2 * dh2a + h - oh)
     return vb
 
@@ -138,9 +153,17 @@ def plot(x, y):
 
 def test():
     ph = np.linspace(0, 14)
-    h2so4(ph)
+    graph = True
+    h2so4(ph, graph=graph)
     # h2so4(4)
+
+def get_value():
+    ph = np.array([0, 1, 2, 2.23, 2.5, 3, 4, 6, 8, 10, 10.5, 11, 11.5, 12])
+    print(len(ph))
+    h2so4(ph, graph=False)
 
 
 if __name__ == "__main__":
-    test()
+    # test()
+    get_value()
+    # h2so4(6)
