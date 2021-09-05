2f35aa5a6056e397231cacd9107fbda23bf3bb36
kaisei <63438515+lemolatoon@users.noreply.github.com>
2021-09-05 16:19:17 +0900

    gly

--- gly_titration.py
+++ gly_titration.py
@@ -9,18 +9,22 @@ va = 50
 kw = 10 ** (-14)
 
 def h2so4(ph: np.ndarray, graph: bool = False):
+    k1 = np.array(10 ** (-2.35))
     k2 = np.array(10 ** (-1.99))
 
-    f = 1.000
-    ca = np.array(0.004) * f
+    beta1 = 10 ** (2.35)
+    beta2 = 10 ** (12.13)
+
+    f = 0.994
+    ca = np.array(0.014) * f
 
     h = 10 ** (-ph)
     oh = kw / h
 
     print(f"\npH:\n{ph}")
 
-    da, dha, dh2a = calc_mol(h, k2)
-    print(f"\nモル分率\nSO4:\n{da * 100}\nHSO4:\n{dha*100}\nH2SO4:\n{dh2a*100}")
+    da, dha, dh2a = calc_mol(h, beta1, beta2)
+    print(f"\nモル分率\nG-:\n{da * 100}\nHG:\n{dha*100}\nH2G+:\n{dh2a*100}")
 
     n = n_bar(dha, dh2a)
 
@@ -30,7 +34,7 @@ def h2so4(ph: np.ndarray, graph: bool = False):
 
     print(f"\n滴下量:\n{vb}")
 
-    alpha = calc_alpha(h, k2)
+    alpha = calc_alpha(h, beta1, beta2)
 
     print(f"\n副反応係数:\n{alpha}")
 
@@ -46,10 +50,11 @@ def h2so4(ph: np.ndarray, graph: bool = False):
         plt.title("vb")
         plt.show()
 
-        plt.plot(ph, da)
-        plt.plot(ph, dha)
-        plt.plot(ph, dh2a)
+        plt.plot(ph, da, label="G-")
+        plt.plot(ph, dha, label="HG")
+        plt.plot(ph, dh2a, label="H2G+")
         plt.title("mol %")
+        plt.legend()
         plt.show()
 
         plt.plot(ph, np.log(alpha))
@@ -61,68 +66,18 @@ def h2so4(ph: np.ndarray, graph: bool = False):
         plt.show()
 
 
-def h2so4_2(ph: np.ndarray, graph: bool = False):
-    k2 = np.array(10 ** (-1.99))
-
-    f = 1.000
-    ca = np.array(0.004) * f
-
-    h = 10 ** (-ph)
-    oh = kw / h
-
-    print(f"\npH:\n{ph}")
-
-    da, dha, dh2a = calc_mol(h, k2)
-    print(f"\nモル分率\nSO4:\n{da}\nHSO4:\n{dha}\nH2SO4:\n{dh2a}")
-
-
-    n = n_bar(dha, dh2a)
-
-    print(f"\n平均プロトン数:\n{n}")
-
-    vb1 = calc_vb(va, ca, dha, dh2a, h, oh, cb)
-    vb1 = calc_vb2(ca, va, n, cb, h, oh)
 
 
 
-    # print(f"\n滴下量:\n{vb}")
+def calc_mol(h, b1, b2):
+    a = calc_alpha(h, b1, b2)
+    dg = 1 / a
 
-    alpha = calc_alpha(h, k2)
+    dhg = b1 * h / a
 
-    print(f"\n副反応係数:\n{alpha}")
+    dh2g = b2 * (h**2) / a
 
-    print(f"\nlog副反応係数:\n{np.log(alpha)}")
-
-    a = calc_a(n, h, oh, ca)
-    # a = 2 - n - (h - oh) / ca
-    vb = a * (ca * va / cb)
-
-    print("vb diff==================")
-    print(vb1 - vb)
-
-    if graph:
-
-        plt.plot(ph, vb)
-        plt.plot(ph, vb1)
-        plt.ylim(0, 20)
-        plt.show()
-        
-        plt.plot(ph, a)
-        plt.ylim(0, 3)
-        plt.show()
-    
-    
-
-
-
-def calc_mol(h, k2):
-    da = k2 / (k2 + h) 
-
-    dha = h / (k2 + h)
-
-    dh2a = np.full_like(da, 0)
-
-    return da, dha, dh2a
+    return dg, dhg, dh2g
 
 def n_bar(dha, dh2a):
     return dha + 2 * dh2a
@@ -138,8 +93,8 @@ def calc_vb2(ca, va, n, cb, h, oh):
     vb = (ca * va * (2 - n) - va * (h - oh)) / (cb + h - oh)
     return vb
 
-def calc_alpha(h, k2):
-    return 1 + h / k2
+def calc_alpha(h, b1, b2):
+    return 1 + b1 * h + b2 * (h ** 2)
 
 def calc_a(n, h, oh, ca):
     return 2- n - (h - oh) / ca
@@ -164,6 +119,6 @@ def get_value():
 
 
 if __name__ == "__main__":
-    # test()
+    test()
     # get_value()
-    h2so4(0.5)
+    # h2so4(0.5)
